059d2c3ee422a925a1ae32f1a3604c185dce55df
Charline Ramelet <charline.ramelet@gmail.com>
2022-11-18 12:12:17 +0100

    Add mocked data score chart

--- sportsee-front/src/components/ScoreChart/ScoreChart.js
+++ sportsee-front/src/components/ScoreChart/ScoreChart.js
@@ -1,45 +1,76 @@
-import './score-chart.scss';
-import { RadialBarChart, RadialBar, PolarAngleAxis } from 'recharts';
+import "./score-chart.scss";
+import { RadialBarChart, RadialBar, Legend } from "recharts";
 
+function ScoreChart({ dataScore }) {
+  const score = dataScore.todayScore;
 
-function ScoreChart({dataScore}) {  
+  /**
+   * Format the score in percentage
+   * @param {object} data
+   * @returns an integer for percentage
+   */
+  function calculatePercent() {
+    return Math.round(score * 100);
+  }
+  const dataScorePercent = calculatePercent(dataScore);
 
-  function formatScore(data){
-    if(data.todayScore){
-        data.score = data.todayScore
-        delete data.todayScore
-        return data
-    }
-}
-formatScore(dataScore);
+  const dataChart = [
+    {
+      name: "score",
+      uv: 5,
+      pv: 0,
+      fill: "#ffc658",
+    },
+  ];
 
-/**
-* Format the score in percentage
-* @param {object} data 
-* @returns an integer for percentage
-*/
-function calculatePercent(data){
-    const score = Number(data.score)
+  const styleChart = {
+    top: 0,
+    left: 350,
+    lineHeight: "24px",
+  };
 
-    return Math.round(score * 100)
-}
-const dataScorePercent = calculatePercent(dataScore)
+  const startAngle = 90;
 
-    return (
+  const endAngle = score * 100 * 3.6 + startAngle;
 
-        <div className='score-chart-container'>
-          <h2 className='score-chart-title'>Score</h2>
-          <RadialBarChart cx="50%" cy="50%" width={100} height={100} innerRadius='90%' startAngle={180} endAngle={-180} data={[dataScore]} >
-            <RadialBar minAngle={15} dataKey='score' fill= '#FF0000' cornerRadius={25} barSize={10} />
-            <PolarAngleAxis type="number" domain={[0, 1]} tick={false} />
-            <circle cx="50%" cy="50%" fill="white" r="85"></circle>
-          </RadialBarChart>
-          <div className="score">
-                <p className="scoreResult">{[`${dataScorePercent}%`, ' de votre objectif']}</p>
-                {/* <p className="scoreText"></p> */}
-            </div>         
-        </div>
-    )
+  return (
+    <div className="score-chart-container">
+      <h2 className="score-chart-title">Score</h2>
+      <RadialBarChart
+        width={250}
+        height={250}
+        cx={150}
+        cy={150}
+        innerRadius={80}
+        outerRadius={100}
+        barSize={20}
+        data={dataChart}
+        startAngle={startAngle}
+        endAngle={endAngle}
+      >
+        <RadialBar
+          label={{ position: "insideStart", fill: "#fff" }}
+          background
+          clockWise
+          dataKey="uv"
+        />
+        <Legend
+          iconSize={10}
+          width={120}
+          height={140}
+          layout="vertical"
+          verticalAlign="middle"
+          wrapperStyle={styleChart}
+        />
+      </RadialBarChart>
+      <div className="score">
+        <p className="scoreResult">
+          {[`${dataScorePercent}%`, " de votre objectif"]}
+        </p>
+        {/* <p className="scoreText"></p> */}
+      </div>
+    </div>
+  );
 }
 
 export default ScoreChart;
